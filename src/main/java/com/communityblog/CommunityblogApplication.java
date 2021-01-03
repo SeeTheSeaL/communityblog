8a32f9438e0984bd9a270b7859d311f6ed7f51d2
Administrator <L18249290950@126.com>
2021-01-03 22:17:41 +0800

    Initial commit

---
+++ src/main/java/com/communityblog/CommunityblogApplication.java
@@ -0,0 +1,13 @@
+package com.communityblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/communityblog/CommunityblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communityblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
